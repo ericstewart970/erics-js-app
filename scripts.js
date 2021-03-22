c0861200c076a98a23949b009e512bec9abfe9e1
ericstewart970 <76798120+ericstewart970@users.noreply.github.com>
2021-03-21 18:10:32 -0600

    excercise 1.3 code

--- scripts.js
+++ scripts.js
@@ -1,10 +1,20 @@
-let pokemonlist = [
-  {name: "balbasure", height: 7, type: ["grass","psychic"]},
-  {name: "caterpie", height: 8, type: ["poison", "water"]}
-];
+// for loop for Exercize 1.3
 
-console.log(pokemonlist);
+let pokemon = [
+    {name: "balbasure (height 0.7)",
+    height: 0.7},
+    {name: "caterpie (height 1.2)",
+    height: 1.2},
+    {name: "Seel (height: 1.9)",
+    height: 1.9}
+    ];
 
-pokemonlist[0];
+    console.log(pokemon[0]);
+    console.log(pokemon[1]);
+    console.log(pokemon[2]);
 
-pokemonlist[1];
+for (let i=0; i < pokemon.length; i++){
+  if (pokemon[i].height > 1.5){
+    console.log(pokemon[i].name + " Wow! That's Big!");
+  }
+}
