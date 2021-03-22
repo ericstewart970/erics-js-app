5c63ea0cd0250a402d777c90960d5be1c344abbb
ericstewart970 <76798120+ericstewart970@users.noreply.github.com>
2021-03-21 18:24:53 -0600

    updated to document write
    also used else if

--- scripts.js
+++ scripts.js
@@ -9,12 +9,12 @@ let pokemon = [
     height: 1.9}
     ];
 
-    console.log(pokemon[0]);
-    console.log(pokemon[1]);
-    console.log(pokemon[2]);
+
 
 for (let i=0; i < pokemon.length; i++){
   if (pokemon[i].height > 1.5){
-    console.log(pokemon[i].name + " Wow! That's Big!");
+    document.write(pokemon[i].name + " Wow! That's Big!");
+  }else if (pokemon[i].height < 1.5) {
+    document.write(pokemon[i].name);
   }
 }
